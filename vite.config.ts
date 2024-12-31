6d7b83c4b6c2c7380db602af84ef591f1f3dd8c7
infinity-atom <50982527+infinity-atom@users.noreply.github.com>
2024-12-31 14:14:02 +1000

    abc

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,12 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
-import { resolve } from "node:path";
 
 export default defineConfig({
     plugins: [vue(), vueDevTools()],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url))
-        }
-    },
-    build: {
-        rollupOptions: {
-            input: {
-                index: resolve(__dirname, "index.html")
-            }
         }
     }
 });
