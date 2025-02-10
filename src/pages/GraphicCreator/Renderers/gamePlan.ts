27d941d32668a0f3563ad37309d40f8173979249
infinity-atom <50982527+infinity-atom@users.noreply.github.com>
2025-02-10 21:31:39 +1000

    Add monarch rules and update gameplan renderer

--- src/pages/GraphicCreator/Renderers/gamePlan.ts
+++ src/pages/GraphicCreator/Renderers/gamePlan.ts
@@ -2,6 +2,7 @@ import { type Ref } from "vue";
 import { parseGamePlan } from "@/parseGamePlan";
 import { maps } from "@/maps";
 import { loadImg } from "../load_img";
+import agents from "@/agents";
 
 const renderGamePlan = async (gamePlan: Ref<string>, ctx: CanvasRenderingContext2D) => {
   const gradient = ctx.createRadialGradient(300, 50, 10, 300, 50, 300);
@@ -12,59 +13,62 @@ const renderGamePlan = async (gamePlan: Ref<string>, ctx: CanvasRenderingContext
   ctx.fillRect(0, 0, 600, 670);
 
   const parsedGamePlan = parseGamePlan(gamePlan.value);
-  const refs: Record<string, string[]> = {};
+  // const refs: Record<string, string[]> = {};
   let cMap: null | string = null;
+  let calloutsDefined = false;
+
+  let callouts: Record<string, string[][]> = {};
+
   for (const line of parsedGamePlan) {
-    switch (line[0]) {
-      case "Map":
-        if (cMap) {
-          throw new Error("Disallowed multiple map declarations");
-        }
+    if (line[0] == "Map") {
+      if (cMap) throw new Error("Disallowed multiple map declarations");
+      if (line.length != 2) throw new Error("Invalid map declaration");
 
-        if (line.length != 2) {
-          throw new Error("Invalid map declaration, Syntax: Map <map name>");
-        }
+      cMap = line[1];
+      const mn = maps.find((a) => a.name == cMap);
+      if (mn) {
+        const img = await loadImg(mn.map);
+        ctx.drawImage(img, 10, 10, 580, 580);
 
-        cMap = line[1];
-        const mn = maps.find((a) => a.name == cMap);
-        if (mn) {
-          const img = await loadImg(mn.map);
-          ctx.drawImage(img, 10, 10, 580, 580);
-        } else {
-          throw new Error(`Unknown map: ${cMap}`);
-        }
+        callouts = mn.callouts.reduce((acc, a) => {
+          acc[a.name] = [];
+          return acc;
+        }, {} as Record<string, string[][]>);
+      } else {
+        throw new Error(`Unknown map: ${cMap}`);
+      }
+    } else if (line[0] == "Callouts") {
+      if (!cMap) throw new Error("Callouts must be declared after map");
+      if (calloutsDefined) throw new Error("Disallowed multiple callout declarations");
 
-        break;
+      const mn2 = maps.find((a) => a.name == cMap);
+      if (mn2) {
+        calloutsDefined = true;
+      }
+    } else if (line[0] == "Agent") {
+      if (line.length != 4) throw new Error("Invalid agent declaration");
+      if (!cMap) throw new Error("Agent must be declared after map");
+      if (!["Attacker", "Defender"].includes(line[1])) throw new Error(`Unknown team: ${line[1]}`);
 
-      case "Callouts":
-        if (!cMap) {
-          throw new Error("Callouts must be declared after map");
-        }
+      callouts[line[3]]?.push([line[2], line[1], cMap, line[3]]);
+    } else {
+      throw new Error(`Unknown command: ${line[0]}`);
+    }
+  }
 
-        if (line.length != 2) {
-          throw new Error("Invalid callouts declaration, Syntax: Callouts [yes/no]");
-        }
-
-        // Draw callouts
-        if (line[1] == "yes") {
-          const mn = maps.find((a) => a.name == cMap);
-          if (mn) {
-            for (const callout of mn.callouts) {
-              ctx.textAlign = "center";
-              ctx.fillStyle = "white";
-              ctx.font = "7px 'Din Next'";
-
-              ctx.fillText(callout.name, (callout.x * 1.16) + 10, (callout.y * 1.16) + 10);
-            }
-          } else {
-            throw new Error(`Unknown map: ${cMap}`);
-          }
-        }
-
-        break;
-
-      default:
-        throw new Error(`Unknown command: ${line[0]}`);
+  // Render callouts if no agents are present on specific callouts
+  for (const [callout, agents] of Object.entries(callouts)) {
+    if (agents.length == 0) {
+      if (cMap && calloutsDefined) {
+        renderCallout(callout, cMap, ctx);
+      }
+    } else {
+      const offsetStart = -(agents.length - 1) / 2;
+      let i = 0;
+      for (const agent of agents) {
+        renderAgent(agent[0], agent[1], agent[2], agent[3], offsetStart + i, ctx);
+        i++;
+      }
     }
   }
 
@@ -73,6 +77,64 @@ const renderGamePlan = async (gamePlan: Ref<string>, ctx: CanvasRenderingContext
   ctx.font = "15px 'Din Next'";
 
   ctx.fillText("VCTtools.net", 600 / 2, 650 + 15);
+};
+
+const renderAgent = async (
+  agent: string,
+  team: string,
+  cMap: string,
+  callout: string,
+  offset: number,
+  ctx: CanvasRenderingContext2D
+) => {
+  const agentSize = 25;
+
+  const agentT = agents.find((a) => a.name == agent);
+  if (!agentT) {
+    throw new Error(`Unknown agent: ${agent}`);
+  } else {
+    // Draw agent
+    const agentImg = await loadImg(agentT.icon);
+    ctx.fillStyle = team == "Attacker" ? "#f34453" : "#32af8a";
+
+    // Get location
+    const mn3 = maps.find((a) => a.name == cMap);
+    if (mn3) {
+      const calloutLoc = mn3.callouts.find((a) => a.name == callout);
+      if (calloutLoc) {
+        ctx.fillRect(
+          (calloutLoc.x * 1.16 + 10 - agentSize / 2) + offset * (agentSize + 2),
+          calloutLoc.y * 1.16 + 10 - agentSize / 2,
+          agentSize,
+          agentSize
+        );
+        ctx.drawImage(
+          agentImg,
+          calloutLoc.x * 1.16 + 10 - agentSize / 2 + offset * (agentSize + 2),
+          calloutLoc.y * 1.16 + 10 - agentSize / 2,
+          agentSize,
+          agentSize
+        );
+      } else {
+        throw new Error(`Unknown callout: ${callout}`);
+      }
+    }
+  }
+};
+
+const renderCallout = (callout: string, cMap: string, ctx: CanvasRenderingContext2D) => {
+  const mn4 = maps.find((a) => a.name == cMap);
+  if (mn4) {
+    const mn5 = mn4.callouts.find((a) => a.name == callout);
+    if (mn5) {
+      const calloutLoc = mn5;
+      if (calloutLoc) {
+        ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+        ctx.font = "10px 'Din Next'";
+        ctx.fillText(callout, calloutLoc.x * 1.16 + 10, calloutLoc.y * 1.16 + 10);
+      }
+    }
+  }
 }
 
 export { renderGamePlan };
