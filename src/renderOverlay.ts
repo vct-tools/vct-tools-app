1f6d24c4efab67a18a09bf7a3221e5ba89bb9c22
infinity-atom <50982527+infinity-atom@users.noreply.github.com>
2025-02-11 14:53:56 +1000

    Setup overlay stuff pt.1

--- src/renderOverlay.ts
+++ src/renderOverlay.ts
@@ -56,6 +56,15 @@ type GameData = {
   defenders: PlayerData[];
 };
 
+const atkC = "#f34453";
+const defC = "#32af8a";
+function bgGradient(ctx: CanvasRenderingContext2D): void {
+  const gradient = ctx.createRadialGradient(1920 / 2, 1080 / 2, 20, 1920 / 2, 1080 / 2, 1080);
+  gradient.addColorStop(0.05, "rgba(33, 52, 66, 0.9)");
+  gradient.addColorStop(1, "rgba(28, 66, 73, 0.9)");
+  ctx.fillStyle = gradient;
+}
+
 export function renderOverlay(
   ctx: CanvasRenderingContext2D,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -65,22 +74,66 @@ export function renderOverlay(
 ): void {
   ctx.clearRect(0, 0, 1920, 1080);
 
-  const text = ["SORRY, PREVIEW UNAVAILABLE", "The overlay preview is still work in progress"];
+  roundWin(ctx, "ROUND WIN", settings.attackerTeamName, "Attack", 42);
+}
 
-  ctx.font = "100px Tungsten";
-  const w1 = ctx.measureText(text[0]).width;
+function roundWin(
+  ctx: CanvasRenderingContext2D,
+  ceromony: string,
+  winningTeamName: string,
+  winningTeamSide: string,
+  roundNum: number
+): void {
+  bgGradient(ctx);
+  const width = 1000;
+  const height = 300;
+  const roundInfoWidth = 250;
+  const roundInfoHeight = 60;
 
-  ctx.font = "60px Din Next";
-  const w2 = ctx.measureText(text[1]).width;
+  ctx.fillRect(1920 / 2 - width / 2, 1080 / 2 - height / 2, width, height);
+  ctx.fillStyle = atkC;
+  ctx.fillRect(
+    1920 / 2 - width / 2 + width / 2 - roundInfoWidth / 2,
+    1080 / 2 - height / 2 - roundInfoHeight / 2,
+    roundInfoWidth,
+    roundInfoHeight
+  );
 
-  ctx.fillStyle = "#01171fdd";
-  ctx.fillRect(1920 / 2 - w2 / 2 - 50, 1080 / 2 - 150, w2 + 100, 300);
-
+  // Round ??
   ctx.fillStyle = "white";
-  ctx.font = "100px Tungsten";
-  ctx.fillText(text[0], 1920 / 2 - w1 / 2, 1080 / 2);
-  ctx.font = "60px Din Next";
-  ctx.fillText(text[1], 1920 / 2 - w2 / 2, 1080 / 2 + 70);
+  ctx.textAlign = "center";
+  ctx.font = "40px 'Din Next'";
+  ctx.fillText(`ROUND ${roundNum}`, 1920 / 2 - width / 2 + width / 2, 1080 / 2 - height / 2 + 12);
+
+  // Ceromony (centered left half of width)
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+  ctx.font = "100px 'Tungsten'";
+  ctx.fillText(
+    ceromony.toUpperCase(),
+    1920 / 2 - width / 2 + width / 4,
+    1080 / 2 - height / 2 + height / 2 + 35
+  );
+
+  // Winning Team Name (centered right half of width)
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+  ctx.font = "100px 'Din Next'";
+  ctx.fillText(
+    winningTeamName.toUpperCase(),
+    1920 / 2 - width / 2 + (width / 4) * 3,
+    1080 / 2 - height / 2 + height / 2 + 10
+  );
+
+  // Winning Team Side (centered right half of width under Winning Team Name)
+  ctx.fillStyle = winningTeamSide.toLowerCase() == "attack" ? atkC : defC;
+  ctx.textAlign = "center";
+  ctx.font = "40px 'Din Next'";
+  ctx.fillText(
+    winningTeamSide.toUpperCase(),
+    1920 / 2 - width / 2 + (width / 4) * 3,
+    1080 / 2 - height / 2 + height / 2 + 50 + 10
+  );
 }
 
 export { type GameData, type PlayerData, type LoadoutData, type AbilityData, type Gun, type Round };
